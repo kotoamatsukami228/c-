5654aea0ddec454ad068e9115d2834631fc0ff12
andrey <andrey228@mail.ru>
2023-09-24 14:44:17 +0300

    Dz_1_8

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dz_1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Четные число от 1 до "+n+": ");
+            int i=2;
+        
+            while(i<=n)
+            {
+                Console.WriteLine(i+ " ");
+                i+=2;
+            }
+
+        }
+    }
+}
